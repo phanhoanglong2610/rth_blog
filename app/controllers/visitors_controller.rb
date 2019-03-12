b38ac3b34342f510a922923ae26c478a7c8b26cd
phanhoanglong2610 <phanhoanglong2610@gmail.com>
2019-03-12 20:28:14 +0700

    UPDATE: add search function add home page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,12 @@
 class VisitorsController < ApplicationController
   def index
-  	@posts = Post.all
+  	search_str = params[:str]
+  	if search_str.present?
+  		@search_str = search_str
+  		@posts = Post.where("title ILIKE ?", "%#{search_str}%").all
+  	else
+  		@posts = Post.all
+  	end
   	@categories = Category.take(10)
   end
 end
